23d896f4ece0f692d2e6f2ebf6ac053b16727e0b
karlaathamiris <karlatn@outlook.com>
2025-01-26 05:21:58 -0300

    feat: create Instructor attributes

--- src/main/java/br/com/alura/ProjetoAlura/user/UserRepository.java
+++ src/main/java/br/com/alura/ProjetoAlura/user/UserRepository.java
@@ -2,7 +2,9 @@ package br.com.alura.ProjetoAlura.user;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     boolean existsByEmail(String email);
